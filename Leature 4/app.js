faf1d753ee56524f4a8d1fc05be51263c93dfee5
Ramnath <ramnath2544@gmail.com>
2025-09-13 14:40:08 +0530

    Send responses to client server

---
+++ Leature 4/app.js
@@ -0,0 +1,18 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Complete Coding</title></head>');
+  res.write('<body><h1>Like / Share / Subscribe</h1></body>');
+  res.write('</html>');
+  res.end();
+
+  // process.exit(); //Stops event loop
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
