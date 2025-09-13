ef959794ecef92556bf1a963dac1913b7f45449f
Ramnath <ramnath2544@gmail.com>
2025-09-13 15:20:37 +0530

    Render routing requests

--- Leature 4/app.js
+++ Leature 4/app.js
@@ -1,13 +1,22 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>Complete Coding</title></head>');
-  res.write('<body><h1>Like / Share / Subscribe</h1></body>');
-  res.write('</html>');
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Complete Coding</title></head>");
+
+  if (req.url === "/") {
+    res.write("<h1>Welcome to Home Page</h1>");
+    res.end();
+  } else if (req.url.toLowerCase() === "/products") {
+    res.write("<h1>Products</h1>");
+    res.end();
+  } else {
+
+  res.write("<body><h1>Like / Share / Subscribe</h1></body>");
+  res.write("</html>");
   res.end();
+  }
 
   // process.exit(); //Stops event loop
 });
