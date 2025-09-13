3090e94bc3ad1a76584382213be9643446175c27
Ramnath <ramnath2544@gmail.com>
2025-09-13 07:31:44 +0530

    Create the first server using node

---
+++ Leature 3/app.js
@@ -0,0 +1,18 @@
+// Simple Node.js Server
+const http = require("http");
+
+// function requestListenter(req, res) {
+//   console.log(req);
+// }
+
+// http.createServer(requestListenter);
+
+ const server = http.createServer((req, res) => {
+  console.log(req);
+});
+
+const PORT =3000
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+})
+ 
