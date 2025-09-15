fe9e4ac5c4454961325bdde967cde572c3a55dda
Ramnath <ramnath2544@gmail.com>
2025-09-15 08:32:40 +0530

    Import and exports module in commonJS(NodeJS)

--- Leature 5/user.js
+++ Leature 5/user.js
@@ -1,7 +1,7 @@
-const http = require("http");
+
 const fs = require("fs");
 
-const server = http.createServer((req, res) => {
+const userRequestHandler = (req, res) => {
   console.log(req.url, req.method);
 
   if (req.url === "/") {
@@ -56,9 +56,8 @@ const server = http.createServer((req, res) => {
   res.write("<body><h1>Like / Share / Subscribe</h1></body>");
   res.write("</html>");
   res.end();
-});
+};
 
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on address http://localhost:${PORT}`);
-});
+module.exports = userRequestHandler;
+
+
