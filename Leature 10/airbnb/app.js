008aa74bb1d58bb7990780bf1f9509c3dca479cf
Ramnath <ramnath2544@gmail.com>
2025-09-21 10:54:25 +0530

    Add Express router in airbnb

---
+++ Leature 10/airbnb/app.js
@@ -0,0 +1,17 @@
+// External Modules
+const express = require('express');
+
+// Local Modules
+const userRouter = require('./routes/userRouter');
+const hostRouter = require('./routes/hostRouter');
+
+const app = express();
+
+app.use(express.urlencoded());
+app.use(userRouter);
+app.use(hostRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on address http://localhost:${PORT}`);
+});
