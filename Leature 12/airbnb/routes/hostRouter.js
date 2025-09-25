97ea9f918c31e9e480c91bed09a5000798b95eae
Ramnath <ramnath2544@gmail.com>
2025-09-25 15:33:05 +0530

    Install the EJS

--- Leature 12/airbnb/routes/hostRouter.js
+++ Leature 12/airbnb/routes/hostRouter.js
@@ -12,8 +12,13 @@ hostRouter.get('/add-home', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'addHome.html'));
 });
 
+const registeredHomes = [];
+
 hostRouter.post('/add-home', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'homeAdded.html'));
+  registeredHomes.push({ homeName: req.body.homeName });
+  console.log('Home Registration successful for:', req.body, req.body.homeName);
 });
 
-module.exports = hostRouter;
+exports.hostRouter = hostRouter;
+exports.registeredHomes = registeredHomes;
