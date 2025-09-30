5f50bf02b4b2c6324785b082fed2fa1d85b6656e
Ramnath <ramnath2544@gmail.com>
2025-09-30 18:25:54 +0530

    Removes data logic and CRUD from routes and add in controller

--- Leature 13/MVC/routes/hostRouter.js
+++ Leature 13/MVC/routes/hostRouter.js
@@ -1,24 +1,12 @@
-// Core Module
-const path = require('path');
-
 // External Module
-const express = require('express');
+const express = require("express");
 const hostRouter = express.Router();
 
 // Local Module
-const rootDir = require("../utils/pathUtil");
+const homesController = require("../controllers/homes");
 
-hostRouter.get("/add-home", (req, res, next) => {
-  res.render('addHome', {pageTitle: 'Add Home to airbnb', currentPage: 'addHome'});
-})
-
-const registeredHomes = [];
-
-hostRouter.post("/add-home", (req, res, next) => {
-  console.log('Home Registration successful for:', req.body);
-  registeredHomes.push(req.body);
-  res.render('homeAdded', {pageTitle: 'Home Added Successfully', currentPage: 'homeAdded'});
-})
+hostRouter.get("/add-home", homesController.getAddHome);
+hostRouter.post("/add-home", homesController.postAddHome);
 
 exports.hostRouter = hostRouter;
-exports.registeredHomes = registeredHomes;
+
