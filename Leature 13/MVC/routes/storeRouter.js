513d14178407d080d720b21acc6925bcd79b45be
Ramnath <ramnath2544@gmail.com>
2026-02-13 10:33:21 +0530

    Rearrange storeRouter

--- Leature 13/MVC/routes/storeRouter.js
+++ Leature 13/MVC/routes/storeRouter.js
@@ -6,8 +6,11 @@ const storeRouter = express.Router();
 const homesController = require('../controllers/storeController');
 
 storeRouter.get('/', homesController.getIndex);
+
 storeRouter.get('/homes', homesController.getHomes);
+
 storeRouter.get('/bookings', homesController.getBookings);
+
 storeRouter.get('/favourites', homesController.getFavouriteList);
 
 module.exports = storeRouter;
