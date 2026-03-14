a8c065d113ef3b90f3a2f252f324bcf3f7d45601
Ramnath <ramnath2544@gmail.com>
2026-03-14 08:24:01 +0530

    Connect to MySQL

--- Leature 15/DB/app.js
+++ Leature 15/DB/app.js
@@ -9,6 +9,15 @@ const storeRouter = require('./routes/storeRouter');
 const hostRouter = require('./routes/hostRouter');
 const rootDir = require('./utils/pathUtil');
 const errorsController = require('./controllers/error');
+const db = require("./utils/databaseUtil");
+const { error } = require('console');
+
+db.execute('SELECT * FROM homes')
+.then(([rows, fields]) => {
+  console.log('Getting from DB', rows);
+}).catch(error => {
+  console.log('Error while reading home records', error);
+})
 
 const app = express();
 
